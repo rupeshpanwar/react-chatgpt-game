74ba195ee0eeac2a574f649a8b7029c42e83bd49
Jon Doe <jondoe@doe.com>
2023-02-04 15:12:19 +0800

    step2-setup mongodb, connect app to db via mongoose

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const uri = 'mongodb+srv://dbadmin:yehtohonahetha@cluster0.4kv2mjn.mongodb.net/?retryWrites=true&w=majority'
+
+// mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+// const db = mongoose.connection;
+
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log("Connected to MongoDB");
+// });
+
+
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+   // console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+connectToMongoDB().then(() => {
+    // Your app code goes here
+    console.log("Connected to MongoDB");
+  });
+  
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
